444419c06cc2ed7d0f69f3b5f01b5eb630f88513
FabiolaParraV <fabiolaparravejar@gmail.com>
2024-09-02 08:27:08 -0400

    create handlers with queryselector to open avatar, profile and cards popups

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 export default function Footer() {
-  <footer className="footer">
-    <p className="footer__copyright">&#169;2024. Fabiola Parra Vejar</p>
-  </footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">&#169;2024. Fabiola Parra Vejar</p>
+    </footer>
+  );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
 import profileAvatar from "../images/profile__image.png";
 
 export default function Main() {
+  function handleEditAvatarClick() {
+    document.querySelector("#popup-avatar-profile").classList.add("popup_open");
+  }
+  function handleEditProfileClick() {
+    document.querySelector("#popup-profile").classList.add("popup_open");
+  }
+  function handleAddPlaceClick() {
+    document.querySelector("#popup-cards").classList.add("popup_open");
+  }
+
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__avatar-container">
+        <div
+          className="profile__avatar-container"
+          onClick={handleEditAvatarClick}
+        >
           <img
             src={profileAvatar}
             alt="espacio con forma circular donde va una foto de perfil del usuario o usuaria"
@@ -19,11 +32,17 @@ export default function Main() {
         <div className="profile__info-container">
           <div className="profile__name-container">
             <h2 className="profile__name">Fabiola Parra</h2>
-            <div className="profile__edit-button"></div>
+            <div
+              className="profile__edit-button"
+              onClick={handleEditProfileClick}
+            ></div>
           </div>
           <p className="profile__description">Estudiante</p>
         </div>
-        <div className="profile__add-button"></div>
+        <div
+          className="profile__add-button"
+          onClick={handleAddPlaceClick}
+        ></div>
       </section>
 
       <section className="cards"></section>
@@ -39,6 +58,123 @@ export default function Main() {
           </div>
         </article>
       </template>
+
+      <section className="popup" id="popup-profile">
+        <div className="popup__container">
+          <div className="popup__close-button"></div>
+          <h2 className="popup__title">Editar perfil</h2>
+          <form
+            name="edit-profile-form"
+            className="popup__form popup__form_profile"
+          >
+            <input
+              type="text"
+              className="popup__form-input popup__form-input_type_name"
+              name="name"
+              id="name"
+              placeholder="Nombre"
+              required
+              minLength="2"
+              maxLength="40"
+            />
+            <span className="popup__form-error-message name-error"></span>
+
+            <input
+              type="text"
+              className="popup__form-input popup__form-input_type_about"
+              name="about"
+              id="about"
+              placeholder="Acerca de mí"
+              required
+              minLength="2"
+              maxLength="200"
+            />
+            <span className="popup__form-error-message about-error"></span>
+            <button type="submit" value="submit" className="popup__form-button">
+              Guardar
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <section className="popup" id="popup-cards">
+        <div className="popup__container">
+          <div className="popup__close-button"></div>
+          <h2 className="popup__title">Nuevo lugar</h2>
+          <form name="add-card-form" className="popup__form popup__form_cards">
+            <input
+              type="text"
+              className="popup__form-input popup__form-input_type_place"
+              id="place-name"
+              name="name"
+              placeholder="Título"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+            <span className="popup__form-error-message place-name-error"></span>
+            <input
+              type="url"
+              className="popup__form-input popup__form-input_type_link"
+              id="image-link"
+              name="link"
+              placeholder="Enlace a la imagen"
+              required
+            />
+            <span className="popup__form-error-message image-link-error"></span>
+            <button type="submit" value="submit" className="popup__form-button">
+              Crear
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <section className="popup" id="popup-photo">
+        <div className="popup__container popup__container_type_popup-photo">
+          <div className="popup__close-button"> </div>
+          <img src=" " className="popup__image" alt="" />
+          <h2 className="popup__subtitle"></h2>
+        </div>
+      </section>
+
+      <section className="popup" id="popup-avatar-profile">
+        <div className="popup__container">
+          <div className="popup__close-button"></div>
+          <h2 className="popup__title">Cambiar foto de perfil</h2>
+          <form
+            name="profile-avatar-form"
+            className="popup__form popup__form_avatar-profile"
+          >
+            <input
+              type="url"
+              className="popup__form-input popup__form-input_type_link"
+              id="avatar-link"
+              name="avatarLink"
+              placeholder="Enlace a la imagen"
+              required
+            />
+            <span className="popup__form-error-message avatar-link-error"></span>
+            <button type="submit" value="submit" className="popup__form-button">
+              Guardar
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <section className="popup" id="popup-delete-confirmation">
+        <div className="popup__container popup__container_type_delete-confirmation">
+          <div className="popup__close-button"></div>
+          <h2 className="popup__title">¿Estás segura/o?</h2>
+          <form
+            name="delete-confirmation-form"
+            className="popup__form popup__form_delete-confirmation"
+          >
+            <button type="submit" value="submit" className="popup__form-button">
+              Sí
+            </button>
+          </form>
+        </div>
+      </section>
     </main>
   );
 }
