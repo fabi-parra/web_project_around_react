db1708490899f2bf5247b3da58413d5897ef97ab
FabiolaParraV <fabiolaparravejar@gmail.com>
2025-01-21 19:35:31 -0300

    create code for delete button classname

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,15 +8,19 @@ export default function Cards({ link, name, likes, onCardClick, card }) {
   }
   const isLiked = card.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = `card__icon_type_like${
-    isLiked ? 'card__icon_type_like-active' : ''
+    isLiked ? "card__icon_type_like-active" : ""
   }`;
+
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonClassName = `${isOwn ? "card__icon_type_delete" : ""}`;
+
   return (
     <article className="card">
       <img alt=" " className="card__image" src={link} onClick={handleClick} />
       <h2 className="card__title">{name}</h2>
-      <div className="card__icon card__icon_type_delete"></div>
+      <div className={cardDeleteButtonClassName}></div>
       <div className="card__like-elements-container">
-        <div className={ cardLikeButtonClassName }></div>
+        <div className={cardLikeButtonClassName}></div>
         <span className="card__like-counter">{likes.length}</span>
       </div>
     </article>
