16d99e126cf56986d72663f999d7651cf06618b1
FabiolaParraV <fabiolaparravejar@gmail.com>
2025-09-16 12:48:05 -0300

    correccion card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-export default function Cards({
+export default function Card({
   link,
   name,
   likes,
