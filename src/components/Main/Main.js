19166ae8cee7a4f1ecf782ed769560d3a7ba3357
FabiolaParraV <fabiolaparravejar@gmail.com>
2025-09-16 12:39:51 -0300

    create directory card inside components in main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Card from "./components/Card";
+import Card from "./components/card/Card";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 export default function Main(props) {
